30b7fc4002c318c8f8e8363e71241536e50b17fb
Nick Goble <nick.goble@dominodatalab.com>
2025-10-29 09:59:54 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/nick_goble/Model-Uploader?executionId=68ffd6b8099f544574840177

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ logger = logging.getLogger(__name__)
 logging.getLogger('werkzeug').setLevel(logging.INFO)
 logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
 
-DOMINO_DOMAIN = os.environ.get("DOMINO_DOMAIN", "")
-DOMINO_API_KEY = os.environ.get("DOMINO_API_KEY", "")
+DOMINO_DOMAIN = os.environ.get("DOMINO_DOMAIN", "fitch.domino-eval.com")
+DOMINO_API_KEY = os.environ.get("DOMINO_USER_API_KEY", "")
 
 logger.info(f"DOMINO_DOMAIN: {DOMINO_DOMAIN}")
 logger.info(f"DOMINO_API_KEY: {'***' if DOMINO_API_KEY else 'NOT SET'}")
@@ -608,9 +608,6 @@ def safe_domino_config():
 def home():
     return render_template("index.html", DOMINO=safe_domino_config())
 
-@app.route("/original")
-def original():
-    return render_template("original_index.html", DOMINO=safe_domino_config())
 
 if __name__ == "__main__":
     # Test API connectivity on startup
